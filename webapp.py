b47cf80f8edf0c1e858219e2de5fca070588f0c2
programming-john <31251360+programming-john@users.noreply.github.com>
2017-11-02 09:06:14 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,11 +1,18 @@
+from flask import Flask, url_for, render_template, request, Markup, flash
 import os
-from flask import Flask, url_for, render_template, request
+import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 def get_state_options():
-    op = Markup("<option value=" +"test>" + "test" + "</option>")
-    return op
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    state = counties[0]["State"]
+    choice = ""
+    for c in counties:
+        choice += Markup("<option value=" + state +">" + state + "</option>")
+        state = c["State"]
+    return choice
 
 @app.route("/")
 def render_main():
